95375bb7518cea7beafc3698981ea3b67d3d811b
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2025-04-20 15:19:48 +0900

    検索フローを大幅改修

--- qwen_websearch_test.py
+++ qwen_websearch_test.py
@@ -1,13 +1,13 @@
 from llama_cpp import Llama
 
-model="Qwen-2.5-32B-Instruct-Q4_K_M.gguf"
+model ="F:\ABEJA-Qwen2.5-32b-Japanese-v0.1-IQ3_M.gguf"
 
-n_ctx=4096
+n_ctx=8192
 llm = Llama(
       model_path=model,
       n_gpu_layers=-1,
       n_ctx=n_ctx,
-       verbose=False,
+      #verbose=False,
 )
 import datetime
 import gguf_DDGWebSearch
@@ -20,19 +20,19 @@ day = str(datetime.datetime.now().year)\
          +str(datetime.datetime.now().hour)\
             +"/"+str(datetime.datetime.now().minute)
 #プロンプト
-prompt = "2024年10月現在の日本の総理大臣をインターネット検索して"
+prompt = "2025年4月現在の日本の総理大臣をインターネットで検索して結果を要約して"
 #ツール実行
-site_summary=gguf_DDGWebSearch.Run(n_ctx,llm,prompt)
+for output in gguf_DDGWebSearch.Run(llm=llm,prompt=prompt,model_type=3,n_ctx=n_ctx):#model_type=3はChatML系
+    pass
+site_summary=output
 #ツールの実行結果
-#2024年10月現在の日本の総理大臣は石破茂氏です。石破氏は自民党総裁選挙2024で選出され、2024年10月1日に正式に総理大臣としての職に就いています。
-#この選挙では、決選投票の末に高市氏を破り、新総裁に選ばれました。
-#石破茂総裁は、10月1日の衆議院本会議で内閣総理大臣に指名され、第214回臨時国会の冒頭で第102代内閣総理大臣に選出されました。
-#また、石破総理は10月4日に第214回国会で所信表明演説を行い、日本の将来に向けた政策を述べています。
+#(['https://www.kantei.go.jp/jp/103/statement/2025/0401kaiken.html', 'https://ja.wikipedia.org/wiki/内閣総理大臣の一覧', 'https://www.kantei.go.jp/jp/103/statement/2025/0101nentou.html'], ['令和7年4月1日 石破内閣総理大臣記者会見 | 総理の演説・記者 ...', '内閣総理大臣の一覧 - Wikipedia', '石破内閣総理大臣 令和7年 年頭所感 - 首相官邸ホームページ'])
+#・まとめ
+#2025年4月現在、日本の総理大臣は石破茂氏です。石破総理は、令和7年4月1日の記者会見で、現在の経済や社会の課題に対する取り組みについて説明しており、特に賃金の引き上げ、物価高 への対応、地方創生、そして少子高齢化への対策などを重点的に進めていることを強調しています。
 
 #システムプロンプト
 system = f"<|im_start|>system\n\
-あなたは文章の理解、要約、ロールプレイ、Web検索でリアルタイムでの情報提供ができるなど様々なタスクをこなせる日本語の大規模言語モデルです。\n\
-言語は日本語です。\n\
+あなたは優秀なアシスタントです\n\
 現在の日付・曜日・時刻はそれぞれ{day}です\n\
 \n\
 **Web検索結果:**\n\
@@ -41,19 +41,17 @@ system = f"<|im_start|>system\n\
 {site_summary}\n\
 **Web検索結果ここまで<|im_end|>"
 
-prompt = system+"\n"+ "<|im_start|>user\n"+"2024年10月現在の日本の総理大臣をインターネット検索して"+"<|im_end|>\n<|im_start|>assistant\n"
-print(prompt)
+send_prompt = system+"\n"+ "<|im_start|>user\n"+prompt+"<|im_end|>\n<|im_start|>assistant\n"
+print(send_prompt)
 #推論
 output = llm(
-           prompt=prompt,
+           prompt=send_prompt,
            max_tokens=1024,
-           temperature = 0.77,
+           temperature = 0.8,
            top_p=0.95, 
            top_k=40, 
-           stop=["<|im_"]
         )
 output= output["choices"][0]["text"]
 print("#最終的な回答\n"+output)
 #最終的な回答
-#2024年10月現在の日本の総理大臣は石破茂氏です。石破氏は2024年10月1日に正式に総理大臣としての職に就いています。
-#また、石破総理は10月4日に第214回国会で所信表明演説を行い、日本の将来に向けた政策を述べています。
+#2025年4月現在、日本の総理大臣は石破茂氏です。石破総理は、経済や社会の課題に対する取り組み、特に賃金の引き上げ、物価高への対応、地方創生、少子高齢化対策などを重点的に進めています。
